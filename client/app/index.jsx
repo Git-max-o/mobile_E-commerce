7d2e8e59cc010b2f610e0941368a4b00b3e124d0
hrimd <hrimd863@email.com>
2025-05-26 12:19:57 +0600

    1st

---
+++ client/app/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className=" bg-blue-500 flex-1 justify-center items-center">
+      <Text className="bg-white  text-2xl">hello world..</Text>
+      <Text className="bg-white  text-2xl">hello world..</Text>
+      <Text className="bg-white  text-2xl">hello world..</Text>
+    </View>
+  );
+}
